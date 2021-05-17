9e8630df670ebcca582ddbfec51fc2ddacabc216
Matthew <43993777+matthew-5pl@users.noreply.github.com>
2021-05-17 10:04:00 +0200

    Add version 1.0

---
+++ UTParse/Parser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UTParse
+{
+    public class Parser
+    {
+        public static ProjectData ParseUTInfo(string utinfo)
+        {
+            List<string> pre = new List<string>();
+            List<string> post = new List<string>();
+            string[] linesUT = utinfo.Split(Environment.NewLine);
+            int currentIndex = 0;
+            string version = "";
+            foreach (string s in linesUT)
+            {
+                if (s.Contains("UNTONE Project Info File"))
+                {
+                    string prefix = "UNTONE Project Info File";
+                    version = s.Substring(prefix.Length + 1, 3);
+                }
+                else if (s.Contains(":") && !string.IsNullOrWhiteSpace(s))
+                {
+
+                    var colonIndex = s.IndexOf(":");
+                    pre.Add(s.Substring(0, colonIndex));
+                    post.Add(s.Substring(colonIndex + 2));
+                    if (currentIndex <= linesUT.Length)
+                    {
+                        currentIndex++;
+                    }
+                }
+            }
+            return new ProjectData(version, int.Parse(post[0]), post[1], post[2], post[3], post[4], post[5], post[6], post[7], post[8]);
+        }
+    }
+}
---
+++ UTParse/ProjectData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace UTParse
+{
+    public class ProjectData //Set up a class to store our parsed data
+    {
+        public string Version { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Edition { get; set; }
+        public string Genre { get; set; }
+        public string Framework { get; set; }
+        public string Language { get; set; }
+        public string Location { get; set; }
+        public string Creator { get; set; }
+        public string Copyright { get; set; }
+
+        public ProjectData(string ver, int id, string nm, string ed, string gnr, string frm, string lang, string loc, string cr, string cc) //Class constructor taking in 10 args
+        {
+            this.Version = ver;
+            this.ID = id;
+            this.Name = nm;
+            this.Edition = ed;
+            this.Genre = gnr;
+            this.Framework = frm;
+            this.Language = lang;
+            this.Location = loc;
+            this.Creator = cr;
+            this.Copyright = cc;
+        }
+
+    }
+}
